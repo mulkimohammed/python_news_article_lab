cda6b712c00a9124c52c86bcb0f2c7e442d73e4f
Mulki <aweismulki@gmail.com>
2026-02-26 12:07:00 +0300

    second commit

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -1,22 +1,25 @@
-with open("news_article.txt", "r", encoding="utf-8") as file:
-    article_text = file.read()
-
+#with open("news_article.txt", "r", encoding="utf-8") as file:
+   # article_text = file.read()
+import re
 
 # Function1:Count specific word
 def count_specific_word(text, word):
-    if not text:
-        return 0  #edge case if text is empty
-
-    words = text.lower().split()  #convert to lowercase and split
-    return words.count(word.lower())
+    if word.lower() in text.lower(): 
+        cleaned_text= re.sub(r"[^\w\s]", '', text)
+        words = cleaned_text.lower().split()  #convert to lowercase and split
+        word_count=words.count(word.lower())
+        return word_count
+    else:
+        return 0
 
 
 # Ask user for word to search
-search_word = input("Enter a word to count: ")
+#search_word = input("Enter a word to count: ")
 
 # Call the function & print result
-print("Word count:", count_specific_word(article_text, search_word))
-
+#print("Word count:", count_specific_word(article_text, search_word))
+word=count_specific_word("This is a test. This is only a test.", "test")
+print (word)
 # Function2:Identify the most common word
 def identify_most_common_word(text):
     if not text:
@@ -30,8 +33,8 @@ def identify_most_common_word(text):
 
     most_common = max(word_count, key=word_count.get)  #get the word with highest count
     return most_common
-print("most common word:",
- identify_most_common_word(article_text))
+# print("most common word:",
+#  identify_most_common_word(article_text))
 
  # Function3:Calculate Average Word Length
 def calculate_average_word_length(text):
@@ -48,8 +51,8 @@ def calculate_average_word_length(text):
     total_length = sum(len(word) for word in words)
     average_length = total_length / len(words)
     return average_length
-print("Average word length:",
-calculate_average_word_length(article_text))
+#print("Average word length:",
+#calculate_average_word_length(article_text))
 
 # Function4: Count no of paragraphs
 def count_paragraphs(text):
@@ -59,7 +62,7 @@ def count_paragraphs(text):
     # Split by empty lines (paragraphs separated by double newlines)
     paragraphs = text.split("\n\n")
     return len(paragraphs)
-print("Number of paragraphs:", count_paragraphs(article_text))
+#print("Number of paragraphs:", count_paragraphs(article_text))
 
 # Function5: Count no of sntcs
 def count_sentences(text):
@@ -72,4 +75,4 @@ def count_sentences(text):
     # Remove empty strings from split
     sentences = [s for s in sentences if s.strip()]
     return len(sentences)
-print("Number of sentences:", count_sentences(article_text))
+#print("Number of sentences:", count_sentences(article_text))
